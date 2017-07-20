246cb97817321d2a8025c8c89f23624b6123d69b
maxwitunski <max.witunski@onbase.com>
2017-07-20 11:10:49 -0400

    Added postman requests and updated runtime to 1.1

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using Library.API.Entities;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+	[Route("api/authors")]
+	public class AuthorsController : Controller
+    {
+		private ILibraryRepository _libraryRepo;
+
+		public AuthorsController(ILibraryRepository libraryRepository)
+		{
+			_libraryRepo = libraryRepository;
+		}
+
+		[HttpGet]
+		public IActionResult GetAuthors()
+		{
+			IEnumerable<Author> authorsFromRepo = _libraryRepo.GetAuthors();
+			return new JsonResult(authorsFromRepo);
+		}
+    }
+}
