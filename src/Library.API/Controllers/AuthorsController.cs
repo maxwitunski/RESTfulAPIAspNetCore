273771a1f7cc04026b883453df1d1be40f82c9e2
maxwitunski <max.witunski@onbase.com>
2017-07-28 10:56:10 -0400

    Finished module 2.

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -1,16 +1,15 @@
-﻿using Library.API.Entities;
+﻿using Library.API.Models;
 using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
 
 namespace Library.API.Controllers
 {
 	[Route("api/authors")]
 	public class AuthorsController : Controller
-    {
+	{
 		private ILibraryRepository _libraryRepo;
 
 		public AuthorsController(ILibraryRepository libraryRepository)
@@ -21,8 +20,20 @@ namespace Library.API.Controllers
 		[HttpGet]
 		public IActionResult GetAuthors()
 		{
-			IEnumerable<Author> authorsFromRepo = _libraryRepo.GetAuthors();
-			return new JsonResult(authorsFromRepo);
+			var authorsFromRepo = _libraryRepo.GetAuthors();
+			var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+			return Ok(authors);
 		}
-    }
+
+		[HttpGet("{id}")]
+		public IActionResult GetAuthor(Guid id)
+		{
+			var authorFromRepo = _libraryRepo.GetAuthor(id);
+			if (authorFromRepo == null)
+				return NotFound();
+
+			var author = Mapper.Map<AuthorDto>(authorFromRepo);
+			return Ok(author);
+		}
+	}
 }
