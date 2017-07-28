b2e226952c3516ad18a5307d4f95e6c443a0b10e
maxwitunski <max.witunski@onbase.com>
2017-07-28 17:24:04 -0400

    Finished module 5.

--- src/Library.API/Entities/Book.cs
+++ src/Library.API/Entities/Book.cs
@@ -4,21 +4,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Library.API.Entities
 {
-    public class Book
-    {
-        [Key]       
-        public Guid Id { get; set; }
+	public class Book
+	{
+		[Key]
+		public Guid Id { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public string Title { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public string Title { get; set; }
 
-        [MaxLength(500)]
-        public string Description { get; set; }
+		[MaxLength(500)]
+		public string Description { get; set; }
 
-        [ForeignKey("AuthorId")]
-        public Author Author { get; set; }
+		[ForeignKey("AuthorId")]
+		public Author Author { get; set; }
 
-        public Guid AuthorId { get; set; }
-    }
+		public Guid AuthorId { get; set; }
+	}
 }
