4054a2af5208f6471cbad34b9832fa41e18638dc
maxwitunski <max.witunski@onbase.com>
2017-07-31 10:15:51 -0400

    Implemented paging, filtering, and searching.

---
+++ src/Library.API/Helpers/AuthorsResourceParameters.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.API.Helpers
+{
+	public class AuthorsResourceParameters
+	{
+		const int maxPageSize = 20;
+		private int _pageSize = 10;
+		public int PageNumber { get; set; } = 1;
+		public int PageSize
+		{
+			get
+			{
+				return _pageSize;
+
+			}
+			set
+			{
+				_pageSize = (value > maxPageSize) ? maxPageSize : value;
+			}
+		}
+		public string Genre { get; set; }
+		public string SearchQuery { get; set; }
+
+	}
+}
